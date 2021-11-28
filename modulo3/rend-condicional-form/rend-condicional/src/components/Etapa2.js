3192f309d403faeb62548009c2b449807065c59f
LucianoRib5 <luciano.eng.soft@outlook.com>
2021-11-28 12:22:59 -0300

    Exercício - Renderização Condicional - atualizado

--- modulo3/rend-condicional-form/rend-condicional/src/components/Etapa2.js
+++ modulo3/rend-condicional-form/rend-condicional/src/components/Etapa2.js
@@ -1,61 +1,73 @@
 import React from 'react';
 import Styled from 'styled-components';
+import PerguntaAberta from './PerguntaAberta';
 
-const ContainerEtapa2 = Styled.div`
+const ContainerEtapa = Styled.div`
     display: flex;
     flex-direction: column;
     align-self: center;
     margin-bottom: 1rem;
 `
-const ContainerFormEtapa2 = Styled.form`
+const ContainerFormEtapa = Styled.form`
     display: flex;
     flex-direction: column;
     align-self: center;
     text-align: center;
     width: 200px;
 `
+// const Form = Styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     margin-bottom: 1rem;
+// `
+// const LabelForm = Styled.label`
+//     padding-bottom:1rem;
+// `
 
 export default class Etapa2 extends React.Component{
     
-    state = {
-        curso: "",
-        unidadeDeEnsino: ""
-    }
+    // state = {
+    //     curso: "",
+    //     unidadeDeEnsino: ""
+    // }
     
-    onChangeCurso = (event) =>{
-        this.setState({curso: event.target.value})
-    }
+    // onChangeCurso = (event) =>{
+    //     this.setState({curso: event.target.value})
+    // }
 
-    onChangeUnidadeDeEnsino = (event) =>{
-        this.setState({unidadeDeEnsino: event.target.value})
-    }
+    // onChangeUnidadeDeEnsino = (event) =>{
+    //     this.setState({unidadeDeEnsino: event.target.value})
+    // }
 
     render(){
         return(
-            <ContainerEtapa2>
+            <ContainerEtapa>
                 <h2>ETAPA 2 - INFORMAÇÕES DO ENSINO SUPERIOR</h2>
-                <ContainerFormEtapa2>
-                    <label for="curso">5. Qual curso?</label>
-                    <input
-                    type = "text"
-                    id = "curso"
-                    name = "curso" 
-                    value = {this.state.curso}
-                    onChange = {this.onChangeCurso}
-                    />
-
-                    <label for="unidadeDeEnsino">6. Qual a unidade de ensino?</label>
-                    <input
-                    type = "text"
-                    id = "unidadeDeEnsino"
-                    name = "unidadeDeEnsino" 
-                    value = {this.state.unidadeDeEnsino}
-                    onChange = {this.onChangeUnidadeDeEnsino}
-                    />
-                </ContainerFormEtapa2>
-
-
-            </ContainerEtapa2>
+                <ContainerFormEtapa>
+                    <PerguntaAberta label={"5. Qual curso?"}/>
+                    <PerguntaAberta label={"6. Qual a unidade de ensino?"}/>
+                    {/* <Form>
+                        <LabelForm for="curso">5. Qual curso?</LabelForm>
+                        <input
+                        type = "text"
+                        id = "curso"
+                        name = "curso" 
+                        value = {this.state.curso}
+                        onChange = {this.onChangeCurso}
+                        />
+                    </Form>
+                    <Form>
+                        <LabelForm for="unidadeDeEnsino">6. Qual a unidade de ensino?</LabelForm>
+                        <input
+                        type = "text"
+                        id = "unidadeDeEnsino"
+                        name = "unidadeDeEnsino" 
+                        value = {this.state.unidadeDeEnsino}
+                        onChange = {this.onChangeUnidadeDeEnsino}
+                        />
+                    </Form> */}
+                </ContainerFormEtapa>
+            </ContainerEtapa>
         )
     }
 }
--- modulo3/rend-condicional-form/rend-condicional/src/components/Etapa3.js
+++ modulo3/rend-condicional-form/rend-condicional/src/components/Etapa3.js
@@ -1,48 +1,71 @@
 import React from 'react';
 import Styled from 'styled-components';
+import PerguntaAberta from './PerguntaAberta';
+import PerguntaFechada from './PerguntaFechada';
 
-const ContainerEtapa3 = Styled.div`
+const ContainerEtapa = Styled.div`
     display: flex;
     flex-direction: column;
     align-self: center;
     margin-bottom: 1rem;
 `
-const ContainerFormEtapa3 = Styled.form`
+const ContainerFormEtapa = Styled.form`
     display: flex;
     flex-direction: column;
     align-self: center;
     text-align: center;
     width: 200px;
 `
+// const Form = Styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     margin-bottom: 1rem;
+// `
+// const LabelForm = Styled.label`
+//     padding-bottom:1rem;
+// `
 
 export default class Etapa3 extends React.Component{
 
-    state = {
-        conclusao: ""
-    }
+    // state = {
+    //     conclusao: ""
+    // }
 
-    onChangeConclusao = (event) =>{
-        this.setState({conclusao: event.target.value})
-    }
+    // onChangeConclusao = (event) =>{
+    //     this.setState({conclusao: event.target.value})
+    // }
 
     render(){
+        
         return(
-            <ContainerEtapa3>
-                <ContainerFormEtapa3>
-                    <h2>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h2>
-                    <label for="conclusao">5. Por que você não terminou um curso de graduação?</label>
-                    <input type = "text" id = "conclusao" name = "conclusao"                  
-                        value = {this.state.conclusao}
-                        onChange = {this.onChangeConclusao}
+            <ContainerEtapa>
+                <h2>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h2>
+                <ContainerFormEtapa>
+                    <PerguntaAberta label={"5. Por que você não terminou um curso de graduação?"}/>
+                    <PerguntaFechada 
+                        label={"6. Você fez algum curso complementar?"} 
+                        opcao1={"nenhum"}
+                        opcao2={"Curso técnico"}
+                        opcao3={"Curso de inglês"}
+                        opcao4={null}
                     />
-                    <label for="cursoComplementar">6. Você fez algum curso complementar?</label>
-                    <select name="cursoComplementar" id="cursoComplementar">
-                        <option value="valor1">nenhum</option>
-                        <option value="valor2">Curso técnico</option>
-                        <option value="valor3">Curso de inglês</option>
-                    </select>
-                </ContainerFormEtapa3>
-            </ContainerEtapa3>
+                    {/* <Form>
+                        <LabelForm for="conclusao">5. Por que você não terminou um curso de graduação?</LabelForm>
+                        <input type = "text" id = "conclusao" name = "conclusao"                  
+                            value = {this.state.conclusao}
+                            onChange = {this.onChangeConclusao}
+                        />
+                    </Form>
+                    <Form>
+                        <LabelForm for="cursoComplementar">6. Você fez algum curso complementar?</LabelForm>
+                        <select name="cursoComplementar" id="cursoComplementar">
+                            <option value="valor1">nenhum</option>
+                            <option value="valor2">Curso técnico</option>
+                            <option value="valor3">Curso de inglês</option>
+                        </select>
+                    </Form> */}
+                </ContainerFormEtapa>
+            </ContainerEtapa>
         )
     }
 }
