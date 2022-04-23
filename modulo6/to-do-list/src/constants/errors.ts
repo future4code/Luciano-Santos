5e8b9b9ab29dfdd8e7f81792fe62615ae86c696a
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-04-23 19:43:17 -0300

    create user

---
+++ modulo6/to-do-list/src/constants/errors.ts
@@ -0,0 +1,3 @@
+export const errors: {[key:string]:{errorCode:number, message:string}} = {
+    UNPROCESSABLE_ENTITY: {errorCode: 422, message: "Um ou mais parâmetros ausentes, verifique a documentação!"}
+};
--- modulo6/to-do-list/src/index.ts
+++ modulo6/to-do-list/src/index.ts
@@ -2,11 +2,49 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import { AddressInfo } from "net";
 import connection from "./connection";
+import { errors } from "./constants/errors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/user", async (req: Request, res: Response) =>{
+    try {
+        const name = req.body.name;
+        const nickname = req.body.nickname;
+        const email = req.body.email;
+
+        if(!name || !nickname || !email){
+            throw new Error(errors.UNPROCESSABLE_ENTITY.message)
+        };
+
+        await connection
+        .insert({
+            name: name,
+            nickname: nickname,
+            email: email
+        })
+        .into("Users");
+
+        res.status(201).send("Success!");
+    } catch (error: any) {
+        switch(error.message){
+            case errors.UNPROCESSABLE_ENTITY.message:
+                res.status(errors.UNPROCESSABLE_ENTITY.errorCode).send(errors.UNPROCESSABLE_ENTITY.message);
+            break;
+        };
+    };
+});
+
+
+
+
+
+
+
+
+
 
 const server = app.listen(process.env.PORT || 3003, () =>{
     if(server){
