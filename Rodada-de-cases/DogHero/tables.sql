22fa2a9065c04c859d4644b0ebefefb54dd5b032
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-07-23 18:38:42 -0300

    Creating DogWalking table

---
+++ Rodada-de-cases/DogHero/tables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS DogWalking (
+    id VARCHAR(255) PRIMARY KEY,
+    status ENUM("FINALIZADO", "EM ANDAMENTO", "AGENDADO") DEFAULT "AGENDADO",
+    schedule_date DATE NOT NULL,
+    price FLOAT NOT NULL,
+    duration ENUM("30", "60") NOT NULL,
+    latitude INT NOT NULL,
+    longitude INT NOT NULL,
+    pets INT NOT NULL,
+    start_time FLOAT NOT NULL,
+    end_time FLOAT NOT NULL
+);
